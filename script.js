a5e62d49883ad773d3f247ba36cc9237b18b925b
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2024-05-27 16:28:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const profileName = document.getElementById('profileName');
+    const profileBio = document.getElementById('profileBio');
+    const name = "khamdihi Dev";
+    const bio = "Programmer, Anime Lovers And Gamers";
+    profileName.textContent = name;
+    profileBio.textContent = bio;
+});

b3116f1925ddc5fd58998aff80169c6a199f389e
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2024-06-16 12:38:43 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const profileName = document.getElementById('profileName');
-    const profileBio = document.getElementById('profileBio');
-    const name = "khamdihi Dev";
-    const bio = "Programmer, Anime Lovers And Gamers";
-    profileName.textContent = name;
-    profileBio.textContent = bio;
-});
